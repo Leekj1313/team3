dddfcb8f2ea72a0b56e2056ecce3d9752ec82248
20231208kh <mimsso@naver.com>
2024-03-11 20:27:57 +0900

    팀3 서블릿 프로젝트 : 게시판 프로그램(테스트용 화면&서블릿)

--- src/main/java/kr/kh/team3/app/controller/BoardInsertServlet.java
+++ src/main/java/kr/kh/team3/app/controller/BoardInsertServlet.java
@@ -8,13 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/")
-public class ServletHere extends HttpServlet {
+@WebServlet("/board/insert")
+public class BoardInsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/view/board/insert.jsp").forward(request, response);
 	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
--- src/main/java/kr/kh/team3/app/controller/BoardListServlet.java
+++ src/main/java/kr/kh/team3/app/controller/BoardListServlet.java
@@ -8,13 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/")
-public class ServletHere extends HttpServlet {
+@WebServlet("/board/list")
+public class BoardListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/view/board/list.jsp").forward(request, response);
 	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
