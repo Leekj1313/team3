a2925312f5d3f56438e21751305ebcdfe25c1fa7
KwonKieun <kgo6625@naver.com>
2024-03-11 18:36:02 +0900

    MemberVO

--- src/main/java/kr/kh/team3/app/model/vo/MemberVO.java
+++ src/main/java/kr/kh/team3/app/model/vo/MemberVO.java
@@ -1,5 +1,22 @@
 package kr.kh.team3.app.model.vo;
 
-public class MemberVO {
+import java.util.Date;
 
+import kr.kh.team3.app.model.vo.MemberVO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberVO {
+	private String me_id;
+	private String me_ms_state;
+	private String me_pw;
+	private String me_email;
+	private String me_name;
+	private String me_phone;
+	private String me_authority; 
+	private int me_fail_count;
+	private Date me_stop;
+	
 }
