c7ee61c7ba303aa955ef208b2a9397ebbf750ad0
Leekj1313 <153248086+Leekj1313@users.noreply.github.com>
2024-03-19 18:50:08 +0900

    게시글 신고 접수 틀

--- team3/src/main/java/kr/kh/team3/app/pagination/CommentCriteria.java
+++ team3/src/main/java/kr/kh/team3/app/pagination/CommentCriteria.java
@@ -8,15 +8,10 @@ import lombok.NoArgsConstructor;
 public class CommentCriteria extends Criteria{
 	
 	private int poNum;
-	
-	private String meId;
 
 	public CommentCriteria(int page, int perPageNum, int poNum) {	
 		super(page, perPageNum);
 		this.poNum = poNum;
 	}
 
-	public CommentCriteria(int page, int i, int poNum, String me_id) {
-		this.me_id = me_id;
-	}
 }
