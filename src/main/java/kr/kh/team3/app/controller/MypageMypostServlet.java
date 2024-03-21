1d67dd28f2bcf8e2d597b67a0e07d87ea4410ad3
LCB <dhtkfkdh2073@naver.com>
2024-03-21 15:05:07 +0900

    콘솔 출력 코드 삭제

--- src/main/java/kr/kh/team3/app/controller/MypageMypostServlet.java
+++ src/main/java/kr/kh/team3/app/controller/MypageMypostServlet.java
@@ -36,7 +36,6 @@ public class MypageMypostServlet extends HttpServlet {
 		Criteria cri = new Criteria(page, 2, type, search, user.getMe_id());
 		//검색어, 검색타입에 맞는 전체 게시글 개수를 가져옴
 		int totalCount = postService.getMyPostTotalCount(cri);
-		System.out.println("totalcaount : " + totalCount);
 		PageMaker pm = new PageMaker(5, cri, totalCount);
 		request.setAttribute("pm", pm);
 		//현재 페이지 정보에 맞는 게시글 리스트를 가져옴
