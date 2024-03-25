1259ebb8c2ea91c10d36dd2c432ebff725d35e3f
LCB <dhtkfkdh2073@naver.com>
2024-03-25 10:20:58 +0900

    1

--- src/main/java/kr/kh/team3/app/controller/MypageMycommentServlet.java
+++ src/main/java/kr/kh/team3/app/controller/MypageMycommentServlet.java
@@ -20,33 +20,27 @@ import kr.kh.team3.app.service.PostServiceImp;
 public class MypageMycommentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private PostService postService = new PostServiceImp();
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String search = request.getParameter("search");
 		String type = request.getParameter("type");
 		int page;
 		try {
 			page = Integer.parseInt(request.getParameter("page"));
-		}catch(Exception e) {
+		} catch (Exception e) {
 			page = 1;
 		}
-		
-		MemberVO user = (MemberVO)request.getSession().getAttribute("user");
-		
-		Criteria cri = new Criteria(page, 2, type, search, user.getMe_id());
-		System.out.println(cri);
-		System.out.println(cri.getPageStart() + ", " + cri.getPerPageNum());
-		//검색어, 검색타입에 맞는 전체 게시글 개수를 가져옴
-		int totalCount = postService.getMyCommentPostTotalCount(cri);
-		System.out.println("totalcount : " + totalCount);
-		PageMaker pm = new PageMaker(5, cri, totalCount);
-		System.out.println(pm.getStartPage());
-		request.setAttribute("pm", pm);
-		//현재 페이지 정보에 맞는 게시글 리스트를 가져옴
-		ArrayList<CommentVO> list = postService.getMyCommentPostList(cri);
-		System.out.println(list);
 
-		request.setAttribute("list", list);//화면에 전송
+		MemberVO user = (MemberVO) request.getSession().getAttribute("user");
+
+		Criteria cri = new Criteria(page, 2, type, search, user.getMe_id());
+		int totalCount = postService.getMyCommentPostTotalCount(cri);
+		PageMaker pm = new PageMaker(5, cri, totalCount);
+		request.setAttribute("pm", pm);
+		ArrayList<CommentVO> list = postService.getMyCommentPostList(cri);
+		request.setAttribute("list", list);// 화면에 전송
 
 		request.getRequestDispatcher("/WEB-INF/view/mypage/mycomment.jsp").forward(request, response);
 	}
